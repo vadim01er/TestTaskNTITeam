4a7edb81139d53cc38927342a48fbe2ce7689ef2
vadim01er <vadim.ershov.01@mail.ru>
2021-06-04 13:54:18 +0300

    Initial commit

---
+++ src/main/java/com/github/vadim01er/testtaskntiteam/TestTaskNtiTeamApplication.java
@@ -0,0 +1,13 @@
+package com.github.vadim01er.testtaskntiteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskNtiTeamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskNtiTeamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/vadim01er/testtaskntiteam/TestTaskNtiTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vadim01er.testtaskntiteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskNtiTeamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
